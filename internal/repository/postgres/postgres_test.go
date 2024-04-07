6a768ed1f15a09928801459c625ae389a943ea32
Abdur Rofi <rofim58@gmail.com>
2024-04-07 14:09:25 +0700

    Update testcontainer

--- internal/repository/postgres/postgres_test.go
+++ internal/repository/postgres/postgres_test.go
@@ -2,15 +2,59 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/jackc/pgx/v4"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
+
+func setupPostgreSQLDB(ctx context.Context) (testcontainers.Container, error) {
+	req := testcontainers.ContainerRequest{
+		Image: "postgres:13-alpine",
+		Name:  "postgresql-test",
+		Env: map[string]string{
+			"POSTGRES_USER":     "username",
+			"POSTGRES_PASSWORD": "password",
+			"POSTGRES_DB":       "database_name",
+		},
+		WaitingFor:   wait.ForListeningPort("5432/tcp"),
+		ExposedPorts: []string{"5432/tcp", "5432/tcp"},
+		Networks:     []string{"bridge"},
+	}
+	// Start a PostgreSQL container
+	postgresContainer, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+		Reuse:            true,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("Unable to start PostgreSQL container: %v", err)
+	}
+	defer postgresContainer.Terminate(ctx)
+
+	return postgresContainer, nil
+}
 
 // TestCRUD is a simple test to test CRUD operations
 func TestCRUD(t *testing.T) {
+	ctx := context.Background()
+	postgresContainer, err := setupPostgreSQLDB(ctx)
+	if err != nil {
+		t.Fatalf("Unable to setup PostgreSQL database: %v", err)
+	}
+	containerHost, err := postgresContainer.Host(ctx)
+	if err != nil {
+		t.Fatalf("Unable to get container host: %v", err)
+	}
+	containerPort, err := postgresContainer.MappedPort(ctx, "5432")
+	if err != nil {
+		t.Fatalf("Unable to get container port: %v", err)
+	}
+	fmt.Printf("Host: %s, Port: %s\n", containerHost, containerPort.Port())
 	// Open a connection to the PostgreSQL database
-	conn, err := pgx.Connect(context.Background(), "postgresql://username:password@localhost:5432/database_name")
+	conn, err := pgx.Connect(context.Background(), "postgresql://username:password@"+containerHost+":"+containerPort.Port()+"/database_name")
 	if err != nil {
 		t.Fatalf("Unable to connect to database: %v", err)
 	}
